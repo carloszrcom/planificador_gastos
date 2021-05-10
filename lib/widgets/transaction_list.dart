90fd95cb487f60b9b961a222fd7316638562ba02
CarlosZR <carlos.z.r.78@gmail.com>
2021-05-10 10:13:26 +0200

    main con MediaQuery y resta el appBar y el status bar.

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,104 +10,102 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 550,
-      child: transactions.isEmpty
-          ? Column(
-              children: [
-                Text(
-                  'No hay transacciones aún',
-                  style: Theme.of(context).textTheme.headline1,
-                  textAlign: TextAlign.center,
+    return transactions.isEmpty
+        ? Column(
+            children: [
+              Text(
+                'No hay transacciones aún',
+                style: Theme.of(context).textTheme.headline1,
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 1,
+              ),
+              Container(
+                height: 150.0,
+                child: Image.asset(
+                  'assets/images/waiting.png',
+                  fit: BoxFit.cover,
                 ),
-                SizedBox(
-                  height: 1,
-                ),
-                Container(
-                  height: 150.0,
-                  child: Image.asset(
-                    'assets/images/waiting.png',
-                    fit: BoxFit.cover,
+              ),
+            ],
+          )
+        : ListView.builder(
+            itemBuilder: (ctx, index) {
+              return Card(
+                elevation: 5,
+                margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    child: Padding(
+                      padding: EdgeInsets.all(6),
+                      child: FittedBox(
+                          child: Text('\$${transactions[index].amount}')),
+                    ),
+                    radius: 30.0,
+                  ),
+                  title: Text(
+                    transactions[index].title,
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  subtitle: Text(
+                    DateFormat.yMMMd().format(transactions[index].date),
+                  ),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    color: Theme.of(context).errorColor,
+                    onPressed: () => deleteTx(transactions[index].id),
                   ),
                 ),
-              ],
-            )
-          : ListView.builder(
-              itemBuilder: (ctx, index) {
-                return Card(
-                  elevation: 5,
-                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      child: Padding(
-                        padding: EdgeInsets.all(6),
-                        child: FittedBox(
-                            child: Text('\$${transactions[index].amount}')),
-                      ),
-                      radius: 30.0,
-                    ),
-                    title: Text(
-                      transactions[index].title,
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                    subtitle: Text(
-                      DateFormat.yMMMd().format(transactions[index].date),
-                    ),
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete),
-                      color: Theme.of(context).errorColor,
-                      onPressed: () => deleteTx(transactions[index].id),
-                    ),
-                  ),
-                );
-                // return Card(
-                //   child: Row(
-                //     children: [
-                //       Container(
-                //         padding: EdgeInsets.all(10),
-                //         margin: EdgeInsets.symmetric(
-                //           vertical: 10,
-                //           horizontal: 15,
-                //         ),
-                //         decoration: BoxDecoration(
-                //           border: Border.all(
-                //             color: Theme.of(context).primaryColor,
-                //             width: 2,
-                //           ),
-                //         ),
-                //         child: Text(
-                //           '\$${transactions[index].amount.toStringAsFixed(2)}',
-                //           // style: Theme.of(context).textTheme.headline6,
-                //           style: TextStyle(
-                //             fontWeight: FontWeight.bold,
-                //             fontSize: 20,
-                //             color: Theme.of(context).primaryColor,
-                //           ),
-                //         ),
-                //       ),
-                //       Column(
-                //         mainAxisAlignment: MainAxisAlignment.start,
-                //         crossAxisAlignment: CrossAxisAlignment.start,
-                //         children: [
-                //           Text(
-                //             transactions[index].title,
-                //             style: Theme.of(context).textTheme.headline6,
-                //           ),
-                //           Text(
-                //             DateFormat('dd-MM-yyyy').format(transactions[index].date),
-                //             style: TextStyle(
-                //               color: Colors.grey,
-                //               fontSize: 12.0,
-                //             ),
-                //           ),
-                //         ],
-                //       )
-                //     ],
-                //   ),
-                // );
-              },
-              itemCount: transactions.length,
-            ),
-    );
+              );
+            },
+            itemCount: transactions.length,
+          );
   }
 }
+
+// return Card(
+//   child: Row(
+//     children: [
+//       Container(
+//         padding: EdgeInsets.all(10),
+//         margin: EdgeInsets.symmetric(
+//           vertical: 10,
+//           horizontal: 15,
+//         ),
+//         decoration: BoxDecoration(
+//           border: Border.all(
+//             color: Theme.of(context).primaryColor,
+//             width: 2,
+//           ),
+//         ),
+//         child: Text(
+//           '\$${transactions[index].amount.toStringAsFixed(2)}',
+//           // style: Theme.of(context).textTheme.headline6,
+//           style: TextStyle(
+//             fontWeight: FontWeight.bold,
+//             fontSize: 20,
+//             color: Theme.of(context).primaryColor,
+//           ),
+//         ),
+//       ),
+//       Column(
+//         mainAxisAlignment: MainAxisAlignment.start,
+//         crossAxisAlignment: CrossAxisAlignment.start,
+//         children: [
+//           Text(
+//             transactions[index].title,
+//             style: Theme.of(context).textTheme.headline6,
+//           ),
+//           Text(
+//             DateFormat('dd-MM-yyyy').format(transactions[index].date),
+//             style: TextStyle(
+//               color: Colors.grey,
+//               fontSize: 12.0,
+//             ),
+//           ),
+//         ],
+//       )
+//     ],
+//   ),
+// );
